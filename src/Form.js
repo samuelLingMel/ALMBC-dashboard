92baebf4fb6317da5da675536d622050ca9705b4
Samuel <lingofthesam@gmail.com>
2020-10-20 00:15:16 +1100

    end of day commit

--- src/Form.js
+++ src/Form.js
@@ -5,10 +5,20 @@ class Form extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      states: ['vic', 'nsw', 'qld'],
+      states: ['qld'],
       fields: []
     };
   };
+
+// remember button to expand all boxes at once
+//   function to show each territories name in states
+// accepts states as an argument
+// first shows bar with field name(and summary?)
+// loops through the length of states
+// this part can be changed depending on parameter
+// if 3 letter code corresponds it returns a div with the name
+// if states[i] corresponds to  3 letter terri code 
+// then show information of field to that terri
 
   showAddState = () => {
     var addState = document.querySelector('.addState');
@@ -17,7 +27,6 @@ class Form extends React.Component {
 
   toggleStates = (e) => {
     if ((this.state.states).includes(e.target.value)) {
-    console.log((e.target.value))
     this.setState({
     states: this.state.states.filter((value) => value !== (e.target.value))
       });
@@ -35,6 +44,23 @@ class Form extends React.Component {
         'gridTemplateColumns': `repeat(${this.state.states.length}, minmax(150px, 1fr)`
     }
 
+    let queensland;
+    let victoria;
+    let newsouthwales;
+
+
+    if ((this.state.states).includes('qld')) {
+        queensland = <div><h3>Queensland</h3><p>QLD field 1 info</p><p>QLD field 2 info</p><p>QLD field 3 info</p></div>
+    }
+    
+    if ((this.state.states).includes('vic')) {
+      victoria = <div><h3>Victoria</h3><p>VIC field 1 info</p><p>VIC field 2 info</p><p>VIC field 3 info</p></div>
+    }
+
+    if ((this.state.states).includes('nsw')) {
+      newsouthwales = <div><h3>New South Wales</h3><p>NSW field 1 info</p><p>NSW field 2 info</p><p>NSW field 3 info</p></div>
+    }
+
     return(
       <div className="form nav-main">
         <aside className='nav-fields'>
@@ -46,27 +72,16 @@ class Form extends React.Component {
         <main>
           <div className='plus-grid'>
             <div className='display-info' style= {styleForGrid}>
-              <div className='col-one'>
-                <h3>Queensland</h3>
-                <p>QLD info</p>
-              </div>
-            
-              <div className='col-two'>
-                <h3>Victoria</h3>
-                <p>VIC info</p>
-              </div>
-
-              <div className='col-three'>
-                <h3>New South Wales</h3>
-                <p>NSW info</p>
-              </div>
+              {queensland}
+              {victoria}
+              {newsouthwales}
             </div>
             <div className='plus' onClick={this.showAddState}>
               +
               <div className='addState'>
                 <ul>
                   <label htmlFor="">Queensland</label>
-                  <input value='qld' type="checkbox"  onClick={this.toggleStates}/>
+                  <input checked value='qld' type="checkbox"  onClick={this.toggleStates}/>
                 </ul>
                 <ul>
                   <label htmlFor="">Victoria</label>
