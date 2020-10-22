865b648dee7564fb89f1989fb7af8972683d9231
Samuel <lingofthesam@gmail.com>
2020-10-23 10:49:16 +1100

    Updates

---
+++ precache-manifest.f42b79d32f26d3e58346e88fac10bc9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4842b988de2b3cee545967188534544",
+    "url": "/ALMBC-dashboard2.0/index.html"
+  },
+  {
+    "revision": "66d957c2017a4a113c19",
+    "url": "/ALMBC-dashboard2.0/static/css/main.efe7cbd9.chunk.css"
+  },
+  {
+    "revision": "95a5fe700685f3361863",
+    "url": "/ALMBC-dashboard2.0/static/js/2.d214c578.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ALMBC-dashboard2.0/static/js/2.d214c578.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66d957c2017a4a113c19",
+    "url": "/ALMBC-dashboard2.0/static/js/main.4b5e6660.chunk.js"
+  },
+  {
+    "revision": "8aa845e0b538f4b722fc",
+    "url": "/ALMBC-dashboard2.0/static/js/runtime-main.17330585.js"
+  }
+]);
