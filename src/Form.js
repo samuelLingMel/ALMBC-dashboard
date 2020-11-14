3f49d52861213592e86a747a2d573b9170b65c29
Samuel <lingofthesam@gmail.com>
2020-11-14 13:22:18 +1100

    i forgot to commit lately.....

--- src/Form.js
+++ src/Form.js
@@ -1,21 +1,85 @@
 import React from 'react';
 import './Form.css'
+import dropDownArrow from './dropdownArrowState.png' 
+import upwardArrow from './dropdownSectionUpward.png' 
+import ReactHtmlParser from 'react-html-parser'; 
+
+const objFilter = (obj, predicate) => 
+  Object.keys(obj)
+    .filter( key => predicate(obj[key]) )
+    .reduce( (res, key) => (res[key] = obj[key], res), {} );
+
 
 class Form extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      states: ['nsw'],
-      fields: ['rar', 'csg', 'csp'],
-      show: {
-        rar: 'open',
-        csg: 'open',
-        csp: 'open'
-      }
+      states: [],
+      fields: [],
+      possibleStates: [],
+      possibleFields: [],
+      show: { },
+      error: null,
+      isLoaded: false,
+      info: {}
+      
     }; 
   };
 
-//------------------------------------------------------------
+//-----------------------------------------------------------
+componentDidMount() {
+  fetch("https://public-api.wordpress.com/rest/v1.1/sites/185189824/posts?filter[parent]=47332")
+    .then(res => res.json())
+    .then(
+      (result) => {
+        
+
+        //parsing 
+        result.posts.forEach( (postJson,index) => {
+
+          // checks fields and will add this one if it isn't already there
+          var fieldCatObj = objFilter( postJson.terms.category, category => category.parent === 47332)
+
+          var fieldName = fieldCatObj[Object.keys(fieldCatObj)].name
+          if (!this.state.possibleFields.includes(fieldName)) {
+            this.setState( prevState => ({
+              possibleFields: [...prevState.possibleFields, fieldName]
+            }))
+          }
+          
+          var stateCatObj = objFilter( postJson.terms.category, category => category.parent === 6418)
+
+          var stateName = stateCatObj[Object.keys(stateCatObj)].name
+          if (!this.state.possibleStates.includes(stateName)) {
+            this.setState( prevState => ({
+              possibleStates: [...prevState.possibleStates, stateName]
+            }))
+          }
+
+          
+          // this.setState( prevState => ({
+          //   info: {...prevState.info, 
+          //     [stateName]: {
+          //       [fieldName]: postJson.content
+          //     }
+          //   }
+          // }));
+          
+          this.setState( prevState => ({
+            isLoaded: true
+          }));
+        })
+      },
+      
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
+      }
+    );
+};
+
 
   getStatesNames = (state) => {
     if (state === 'nsw') {
@@ -37,21 +101,42 @@ class Form extends React.Component {
     }
   }
 
+  collapseField = (target, code) => {
+    this.state.show[`${code}`] = 'closed'
+    console.log(this.state.show[`${code}`])
+  }
+
+  openField = () => {
+
+  }
+
   getFieldNames = (fieldCode) => {
     if (fieldCode === 'rar') {
-      return(<div className='field-title'>Restrictions and Roadmaps</div>)
+      return(<div className='field-title'>Restrictions and Roadmaps<img src={dropDownArrow} className = 'arrows' height='20px' onClick ={this.collapseField.bind(this, 'rar')} /></div>);
     } else if (fieldCode === 'csg') {
-      return(<div className='field-title'>COVID Safe GuideLines</div>)
+      return(<div className='field-title'>COVID Safe GuideLines<img src={upwardArrow} className = 'arrows' height='20px'/></div>);
     } else if (fieldCode === 'csp') {
-      return(<div className='field-title'>COVID Safe Plans</div>)
+      return(<div className='field-title'>COVID Safe Plans</div>);
     }
   }
 
   getInfo = (field, state) => {
     
-    if (field === 'rar') {
-      if (state === 'nsw') {
-        return('Restrictions and Roadmaps NSW Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
+    if (field == 'Current Restrictions / Road to Recovery Stage / Step / Phase') {
+      if (state == 'New South Wales') {
+        const reactState = this.state;
+        const info = reactState.info
+        if (this.state.error) {
+          return <div>Error: {this.error.message}</div>;
+        } else if (reactState.isLoading) {
+          return <div>Loading...</div>;
+        } else {
+          return(
+          
+            <div>{ReactHtmlParser (info)} </div>
+
+          )
+        }
       } else if (state === 'vic') {
         return('Restrictions and Roadmaps VIC Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
       } else if (state === 'qld') {
@@ -321,11 +406,11 @@ class Form extends React.Component {
 
     let stateNames = []
 
-    thisState.states.forEach( (stateCode,index) => {
+    thisState.states.forEach( (stateName,index) => {
       if (index % 2 === 0) {
-        stateNames.push(<div className='state-title'>{this.getStatesNames(stateCode)}</div>)
+        stateNames.push(<div className='state-title'>{stateName}</div>)
       } else {
-        stateNames.push(<div className='state-title gray-bgc'>{this.getStatesNames(stateCode)}</div>)
+        stateNames.push(<div className='state-title gray-bgc'>{stateName}</div>)
       }
 
     })
---
+++ test-cms/my-app/frontity.settings.js
@@ -0,0 +1,57 @@
+const settings = {
+  "name": "my-app",
+  "state": {
+    "frontity": {
+      "url": "https://test.frontity.org",
+      "title": "Test Frontity Blog",
+      "description": "WordPress installation for Frontity development"
+    }
+  },
+  "packages": [
+    {
+      "name": "@frontity/mars-theme",
+      "state": {
+        "theme": {
+          "menu": [
+            [
+              "Home",
+              "/"
+            ],
+            [
+              "Nature",
+              "/category/nature/"
+            ],
+            [
+              "Travel",
+              "/category/travel/"
+            ],
+            [
+              "Japan",
+              "/tag/japan/"
+            ],
+            [
+              "About Us",
+              "/about-us/"
+            ]
+          ],
+          "featured": {
+            "showOnList": false,
+            "showOnPost": false
+          }
+        }
+      }
+    },
+    {
+      "name": "@frontity/wp-source",
+      "state": {
+        "source": {
+          "api": "https://public-api.wordpress.com/rest/v1.1/sites/testsite123398761387.wordpress.com/posts/3"
+        }
+      }
+    },
+    "@frontity/tiny-router",
+    "@frontity/html2react"
+  ]
+};
+
+export default settings;
